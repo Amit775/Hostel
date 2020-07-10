5b2fc0bd86093bf6126ccc4708ed7ca1f5a6c711
Amit Bublil <ambuex@gmail.com>
2020-07-11 00:31:22 +0300

    sample for deploy on server

---
+++ HostelServer/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HostelServer
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class UploadController : ControllerBase
+	{
+		// GET api/<ValuesController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		[HttpPost]
+		public async void PostScript([FromBody] IFormFile file)
+		{
+			string filename = file.Name;
+
+			using FileStream output = System.IO.File.Create($"scripts/{filename}");
+			await file.CopyToAsync(output);
+		}
+
+		[HttpPost]
+		public void PostIcon([FromBody] string value)
+		{
+		}
+
+		// PUT api/<ValuesController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<ValuesController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
--- hostel/deploy.js
+++ hostel/deploy.js
@@ -2,14 +2,21 @@ var fs = require('fs');
 
 const project = 'hostel';
 
-const files = ['main-es5.js', 'main-es5.js.map', 'main-es2015.js', 'main-es2015.js.map'];
+const files = ['main-es2015.js', 'main-es2015.js.map'];
 const distPath = `./dist/${project}/`;
 const destPath = `../main/src/assets/elements/${project}/`;
 
+const formData = new FormData();
+
 files.forEach(name => {
+	formData.append(name, fs.createReadStream(`${distPath}${name}`));
 	fs.copyFile(`${distPath}${name}`, `${destPath}${name}`, (err) => {
 		if (err) throw err;
 	});
 });
 
-console.log(`${project} deployed successfully`);
+fetch(`http://localhost:44395/script/${project}`, { method: 'POST', body: formData }).then(
+	console.log(`${project} deployed successfully`),
+	console.error('something went wrong')
+)
+
