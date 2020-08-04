cd90e405ca8975762cb0fa89d17510b1312fee7c
Amit Bublil <ambuex@gmail.com>
2020-08-05 02:15:15 +0300

    remove peterpan

--- peter-pan/projects/never-land/src/lib/leaflet.type.ts
+++
@@ -1,3 +0,0 @@
-import * as L from 'leaflet';
-
-export class MapInstance extends L.Map { }
--- peter-pan/projects/never-land/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of never-land
- */
-
-export * from './lib/never-land.service';
-export * from './lib/never-land.component';
-export * from './lib/never-land.module';
